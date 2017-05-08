eb44c7802cc7530e13028ed1e5de1dabc2204edc
= <=>
2017-05-08 19:03:58 +0800

    add MOVE,GET,PUT command new

--- .react-playground/index.js
+++
@@ -1,8 +0,0 @@
-
-        import React from 'react';
-        import ReactDom from 'react-dom';
-        import Playground from 'react-playground-kit';
-        import * as module from '/d:/Work/CRANEUI/components/wmsack.jsx';
-
-        ReactDom.render(React.createElement(Playground, { module }), document.getElementById('app'));
-    
